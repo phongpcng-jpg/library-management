20ba2271290419df7c79e3db704335cd7539c8eb
Thomas Phong <ino.thomas@outlook.com>
2026-07-17 17:26:33 +0700

    chore: create upload cloudinary message constant
    - Session08: Exercise01

---
+++ src/main/java/com/example/library_management/message/UploadCloudinaryMessage.java
@@ -0,0 +1,8 @@
+package com.example.library_management.message;
+
+public final class UploadCloudinaryMessage {
+
+    public static final String UPLOAD_CLOUDINARY =
+        "Lỗi khi tải file lên cloudinary.";
+
+}
