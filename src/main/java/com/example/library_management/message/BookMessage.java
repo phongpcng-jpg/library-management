c39db9368ea01e8943a88a9f36c2a95507c6dd40
Thomas Phong <ino.thomas@outlook.com>
2026-07-17 15:58:31 +0700

    chore: create book message constant
    - Session08: Exercise01

---
+++ src/main/java/com/example/library_management/message/BookMessage.java
@@ -0,0 +1,17 @@
+package com.example.library_management.message;
+
+public final class BookMessage {
+
+    public static final String TITLE_NOT_BLANK = 
+        "Tiêu đề sách không được để trống.";
+
+    public static final String AUTHOR_NOT_BLANK = 
+        "Tên tác giả không được để trống.";
+
+    public static final String STOCK_POSITIVE = 
+        "Số lượng sách tồn kho phải lớn hơn hoặc bằng 0.";
+
+    public static final String COVER_IMAGE_EXTENSION = 
+        "Tập tin ảnh bìa phải có đuôi '.jpg', '.png', '.jpeg'.";
+
+}
