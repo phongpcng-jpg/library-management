cd9a30388145d53a9a76531f1acdf4d975b1ba11
Thomas Phong <ino.thomas@outlook.com>
2026-07-17 14:23:39 +0700

    chore: create common message constant
    - Session08: Exercise01

---
+++ src/main/java/com/example/library_management/message/CommonMessage.java
@@ -0,0 +1,8 @@
+package com.example.library_management.message;
+
+public final class CommonMessage {
+
+    public static final String EXCEPTION_HANDLE =
+            "Lỗi hệ thống không xác định, vui lòng tìm hiểu lại.";
+
+}
